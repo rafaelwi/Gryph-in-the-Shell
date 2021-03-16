79046e735ad0488034971d3e8af9dac8ece9c0b9
rafaelwi <rafwiskailnicki@gmail.com>
2021-03-15 20:05:36 -0400

    Added background and button to take player to bttle scene

--- src/commonMain/kotlin/scenes/MapScene.kt
+++ src/commonMain/kotlin/scenes/MapScene.kt
@@ -4,20 +4,34 @@ import com.soywiz.korge.input.mouse
 import com.soywiz.korge.scene.Scene
 import com.soywiz.korge.view.*
 import com.soywiz.korim.color.Colors
+import com.soywiz.korim.format.readBitmap
 import com.soywiz.korim.text.TextAlignment
+import com.soywiz.korio.file.std.resourcesVfs
 import com.soywiz.korma.geom.Anchor
+import kotlinx.coroutines.MainScope
 
 class MapScene : Scene() {
+    val deviceWidth : Int = MainModule.size.width
+    val deviceHeight : Int = MainModule.size.height
+
     // Entrypoint
     override suspend fun Container.sceneInit() {
-        text("Map Scene", textSize = 100.0, alignment = TextAlignment.TOP_CENTER) {
-            position(MainModule.size.width / 2.0, 0.0)
+        // Background
+        // For the atlas/tiling background: https://github.com/korlibs/korge-samples/tree/master/samples/atlas
+        image(resourcesVfs["map\\grass.png"].readBitmap()) {
+            setSizeScaled(deviceWidth.toDouble(), deviceHeight.toDouble())
         }
+
+
+        text("Map Scene", textSize = 100.0, alignment = TextAlignment.TOP_CENTER, color = Colors.BLACK) {
+            position(deviceWidth / 2.0, 0.0)
+        }
+
 
         // Settings button
         solidRect(100, 100, Colors.SILVER) {
             anchor(Anchor.TOP_RIGHT)
-            position(MainModule.size.width, 0)
+            position(deviceWidth, 0)
             mouse {
                 over {
                     tint = Colors.SLATEGRAY
@@ -34,9 +48,30 @@ class MapScene : Scene() {
         }
 
         // Battle button
+        image(resourcesVfs["map\\placemark_incomplete.png"].readBitmap()){
+            setSizeScaled(256.0, 142.0)
+            anchor(Anchor.MIDDLE_CENTER)
+            position(deviceWidth / 2, deviceHeight / 2)
+            mouse {
+                over {
+                    tint = Colors.ORANGERED
+                }
+
+                out {
+                    tint = Colors.ORANGE
+                }
+
+                onClick {
+                    sceneContainer.changeTo(BattleScene::class)
+                }
+            }
+
+        }
+
+        /*
         solidRect(100, 100, Colors.ORANGE) {
             anchor(Anchor.MIDDLE_CENTER)
-            position(MainModule.size.width / 2, MainModule.size.height / 2)
+            position(deviceWidth / 2, deviceHeight / 2)
             mouse {
                 over {
                     tint = Colors.ORANGERED
@@ -51,5 +86,8 @@ class MapScene : Scene() {
                 }
             }
         }
+         */
     }
+
+
 }
