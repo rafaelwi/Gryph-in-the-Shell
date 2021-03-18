3cca25999006e911360e9859d14d0d53f8fb87a7
rafaelwi <rafwiskailnicki@gmail.com>
2021-03-18 14:03:50 -0400

    Updated korge plugin import to new style

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,31 @@
-import com.soywiz.korge.gradle.*
+plugins {
+	id("com.soywiz.korge") version "2.0.7.1"
+	application
+}
 
+repositories {
+	mavenCentral()
+	jcenter()
+	google()
+}
+
+dependencies {
+	implementation("com.beust:klaxon:5.5") // doesn't work
+}
+
+korge {
+	id = "cis4030.gis.gis"
+	name = "Gryph in the Shell"
+
+	// To enable all targets at once: targetAll()
+	// To enable targets based on properties/environment variables: targetDefault()
+	targetJvm()
+	targetAndroidIndirect()
+	targetAndroidDirect()
+}
+
+/* IMPORTANT: Old buildscript
+import com.soywiz.korge.gradle.*
 buildscript {
 	val korgePluginVersion: String by project
 
@@ -9,25 +35,11 @@ buildscript {
 		maven { url = uri("https://plugins.gradle.org/m2/") }
 		mavenCentral()
 		google()
+		jcenter()
 	}
 	dependencies {
 		classpath("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:$korgePluginVersion")
 	}
 }
-
 apply<KorgeGradlePlugin>()
-
-korge {
-	id = "cis4030.gis.gis"
-	name = "Gryph in the Shell"
-	supportSwf()
-// To enable all targets at once: targetAll()
-
-// To enable targets based on properties/environment variables: targetDefault()
-	targetJvm()
-	targetJs()
-	targetDesktop()
-	targetIos()
-	targetAndroidIndirect()
-	targetAndroidDirect()
-}
+*/
