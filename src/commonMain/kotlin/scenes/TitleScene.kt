0ac1f1281fbd4afaf74dae71cc433ee37f79c076
rafaelwi <rafwiskailnicki@gmail.com>
2021-04-07 11:34:26 -0400

    Added byline & cleanup

--- src/commonMain/kotlin/scenes/TitleScene.kt
+++ src/commonMain/kotlin/scenes/TitleScene.kt
@@ -21,6 +21,7 @@ import com.soywiz.korio.async.launchImmediately
 import com.soywiz.korio.file.std.resourcesVfs
 import com.soywiz.korma.geom.Anchor
 import com.soywiz.korma.interpolation.Easing
+import com.soywiz.korui.layout.Position
 import constants.DebugStatus
 import kotlinx.coroutines.Job
 import map.GameMapFactory
@@ -30,11 +31,10 @@ class TitleScene : Scene() {
     private lateinit var title : Image
     private lateinit var bg : Image
     private lateinit var start : Image
-
+    private lateinit var byline : Text
 
     // Entrypoint
     override suspend fun Container.sceneInit() {
-
         bg = image(resourcesVfs["title\\title_bg.png"].readBitmap()) {
             setSize(this.width, MainModule.size.height / 1.0)
         }
@@ -57,18 +57,23 @@ class TitleScene : Scene() {
                 }
             }
         }
+
+        byline = text("(c) 2021 Eric Min Park & Rafael Wiska-Ilnicki", textSize = 50.0, alignment = TextAlignment.BOTTOM_LEFT) {
+            position(24.0, (MainModule.size.height / 1.0) - 24)
+        }
     }
 
-    // Comment this function out if the fades are getting annoying while testing
     override suspend fun sceneAfterInit() {
         if (PLAY_ANIMATIONS == DebugStatus.ON) {
             title.alpha = 0.0
             bg.alpha = 0.0
             start.alpha = 0.0
+            byline.alpha = 0.0
 
             bg.tween(bg::alpha[1.0], time = 1.seconds)
             delay(1.seconds)
             title.tween(title::alpha[1.0], time = 2.seconds)
+            byline.tween(byline::alpha[1.0], time = 0.25.seconds)
             delay(0.5.seconds)
             start.tween(start::alpha[1.0], time = 1.seconds)
         }
@@ -76,6 +81,7 @@ class TitleScene : Scene() {
 
     private suspend fun startOnClickAnimation() {
         if (PLAY_ANIMATIONS == DebugStatus.ON) {
+            byline.alpha = 0.0
             start.tween(start::alpha[0.0], time = 1.seconds)
             title.tween(title::y[MainModule.size.height / 2.0], time = 0.5.seconds)
             title.tween(title::scale[10], time = 1.seconds, easing = Easing.EASE_IN_QUAD)
